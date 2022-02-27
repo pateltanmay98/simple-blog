5ec7014cc7e83f07c3b42017297285a6e4397790
unknown <patelth1998@gmail.com>
2022-02-27 16:21:20 +0530

    27 02 2022

--- myblog/views.py
+++ myblog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
@@ -15,3 +16,14 @@ class HomeView(ListView):
 class ArticleDetailView(DetailView):
     model = Post
     template_name = "article_details.html"
+
+class AddPostView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = "add_post.html"
+    #fields = '__all__'
+
+class UpdatePostView(UpdateView):
+    model = Post
+    template_name = 'update_post.html'
+    fields = ['title', 'title_tag', 'body']
